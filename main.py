ad6cfbde8817e9f86b42262e3867fc74a1ad22c8
simonuks221 <simonasriauka@gmail.com>
2021-05-23 02:11:17 +0300

    Color vizualization on cube

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def DisplayOrientationLines():
     ax.text(0, 4.5, 0, "Y", (0, 1, 0), color = 'g')
     ax.text(0, 0, 4.5, "Z", (0, 0, 1), color = 'b')
 
-def DisplaySquare(startX, startY, startZ, direction):
+def DisplaySquare(startX, startY, startZ, direction, col = 'k'):
     x, y = 0, 0
     if direction == "x":
+        x = np.meshgrid([startX, startX + 1])
         y = np.meshgrid([startY, startY])
         z = np.array([[startZ],[startZ + 1]])
     elif direction == "y":
@@ -26,35 +27,37 @@ def DisplaySquare(startX, startY, startZ, direction):
         x = np.array([[startX, startX], [startX + 1, startX + 1]])
         y = np.array([[startY, startY + 1], [startY, startY + 1]])
         z = np.array([[startZ],[startZ]])
-    ax.plot_surface(x, y, z, antialiased = False, linewidth = 0, color = 'r')
+    ax.plot_surface(x, y, z, antialiased = False, linewidth = 0, color = col)
     
 
-def DisplayRubiksCube():
-    x = [0,3, 5]
-    y = [0, 1, 3]
-
-    print(x)
-
-    xx = np.meshgrid(x)
-    yy = np.meshgrid(y)
-    zz = np.array([[3], [5]])
-    
-    for a in range(1, 4):
-        for b in range(1, 4):  
-            DisplaySquare(a, 1, b, "x")
-            DisplaySquare(a, 4, b, "x")
-            DisplaySquare(1, a, b, "y")
-            DisplaySquare(4, a, b, "y")
-            DisplaySquare(a, b, 1, "z")
-            DisplaySquare(a, b, 4, "z")
-            
-
-
+def DisplayRubiksCube(cubeArray):
+    for c in range(0, 6):
+        for a in range(0, 3):
+            for b in range(0, 3):
+                if c == 0:
+                    DisplaySquare(a, b, 3, "z", cubeArray[b, a + c * 3])
+                elif c == 1:
+                    DisplaySquare(0, a, b, "y", cubeArray[b, a + c * 3])
+                elif c == 2:
+                    DisplaySquare(a, 0, b, "x", cubeArray[b, a + c * 3])
+                elif c == 3:
+                    DisplaySquare(3, a, b, "y", cubeArray[b, a + c * 3])
+                elif c == 4:
+                    DisplaySquare(a, 3, b, "x", cubeArray[b, a + c * 3])
+                else:
+                    DisplaySquare(a, b, 0, "z", cubeArray[b, a + c * 3])
 
 #ax.plot(x, y, z, label = "Kubas lol", zdir = 'z')
 
+cubeArray = np.empty((3, 18), dtype = str)
+colorArray =["w", "c", "g", "r" ,"y", "b"]
+for a in range(0, 6):
+    for x in range(0, 3):
+        for y in range(0, 3):
+            cubeArray[y, x + a * 3] = colorArray[a]
+
 DisplayOrientationLines();
-DisplayRubiksCube();
+DisplayRubiksCube(cubeArray);
 
 ax.set_xlim(0, 5)
 ax.set_ylim(0, 5)
